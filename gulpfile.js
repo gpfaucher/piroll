73021c39e7b8a561892812fa5a21f93b075777c2
Gabriel Faucher <gpfaucher@gmail.com>
2018-08-01 06:34:59 +0200

    added main grid layout

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ var rename = require('gulp-rename')
 
 gulp.task('css', function(){
 	gulp.src('styles/**/**/**/*.css')
-        .pipe(minifyCSS())
-        .pipe(rename('style.min.css'))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(gulp.dest('dist'))
+		.pipe(minifyCSS())
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+		.pipe(concat('style.min.css'))
+		.pipe(gulp.dest('dist'))
 	gulp.watch('styles/**/**/**/*.css', ['css'])
 });
 
